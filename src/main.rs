a5d48c194167566e467d82c8ceeff03346e66d85
martyr00 <leonyvb04@gmail.com>
2022-05-14 01:00:45 +0300

    Start ddb_data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod api;
+mod repository;
 
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use api::task::get_task;
+use repository::ddb::DDBRepository;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -9,10 +11,23 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    let config = aws_config::load_from_env().await;
+
     HttpServer::new(move || {
+        let ddb_repo: DDBRepository = DDBRepository::init(
+            String::from("task"),
+            config.clone()
+        );
+        let ddb_data = Data::new(
+            ddb_repo
+        );
+
         let logger = Logger::default();
 
-        App::new().wrap(logger).service(get_task)
+        App::new()
+            .wrap(logger)
+            .app_data(ddb_data)
+            .service(get_task)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
