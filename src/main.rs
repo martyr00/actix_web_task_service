a794fcedf472e89d66deb8e12054cb9eb7a48c7a
martyr00 <leonyvb04@gmail.com>
2022-05-15 19:27:32 +0300

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod api;
-mod repository;
 mod model;
+mod repository;
 
+use crate::api::task::{complete_task, fail_task, get_task, pause_task, start_task, submit_task};
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
-use api::task::get_task;
 use repository::ddb::DDBRepository;
 
 #[actix_web::main]
@@ -15,22 +15,21 @@ async fn main() -> std::io::Result<()> {
     let config = aws_config::load_from_env().await;
 
     HttpServer::new(move || {
-        let ddb_repo: DDBRepository = DDBRepository::init(
-            String::from("task"),
-            config.clone()
-        );
-        let ddb_data = Data::new(
-            ddb_repo
-        );
-
+        let ddb_repo: DDBRepository = DDBRepository::init(String::from("task"), config.clone());
+        let ddb_data = Data::new(ddb_repo);
         let logger = Logger::default();
 
         App::new()
             .wrap(logger)
             .app_data(ddb_data)
             .service(get_task)
+            .service(submit_task)
+            .service(fail_task)
+            .service(pause_task)
+            .service(start_task)
+            .service(complete_task)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8082))?
     .run()
     .await
 }
--- src/model/task.rs
+++ src/model/task.rs
@@ -1,8 +1,6 @@
-use std::fmt::format;
-use std::os::macos::raw::stat;
 use serde::Serialize;
+use strum_macros::{Display, EnumString};
 use uuid::Uuid;
-use strum_macros::{EnumString, Display};
 
 #[derive(Serialize, EnumString, Display, Eq, PartialEq)]
 pub enum TaskState {
@@ -10,54 +8,38 @@ pub enum TaskState {
     InProgress,
     Completed,
     Paused,
-    Failed
+    Failed,
 }
 
 #[derive(Serialize)]
-    pub struct Task {
+pub struct Task {
     pub user_uuid: String,
     pub task_uuid: String,
     pub task_type: String,
     pub state: TaskState,
     pub source_file: String,
-    pub result_file: Option<String>
+    pub result_file: Option<String>,
 }
 
 impl Task {
     pub fn new(user_uuid: String, task_type: String, source_file: String) -> Task {
+        let uuid = Uuid::new_v4().to_string();
+        println!("HERE ID {}", &uuid);
         Task {
             user_uuid,
-            task_uuid: Uuid::new_v4().to_string(),
+            task_uuid: uuid,
             task_type,
             state: TaskState::NotStarted,
             source_file,
-            result_file: None
+            result_file: None,
         }
     }
 
     pub fn get_global_id(&self) -> String {
-        format!("{}_{}", self.user_uuid, self.task_uuid);
+        return format!("{}_{}", self.user_uuid, self.task_uuid);
     }
 
     pub fn can_transition_to(&self, state: &TaskState) -> bool {
         self.state != *state
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
